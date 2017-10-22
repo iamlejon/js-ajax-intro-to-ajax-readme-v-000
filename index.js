7bf868a203baaf9a2102b2d9a9bdec447b1d9e06
Dominique McCloud <niquelejon@gmail.com>
2017-10-22 20:38:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function showRepositories(event, data) {
   repoList += "</ul>"
   document.getElementById("repositories").innerHTML = repoList
 }
- 
+
 function getRepositories() {
   const req = new XMLHttpRequest()
   req.addEventListener("load", showRepositories);
